5488eee375207379b59eb5ab1fb6694256a15d3f
Roman Horokhovatskyy <roman.horokhovatskyy@gmail.com>
2016-07-06 14:20:13 +0200

    Open links it _blank

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -390,6 +390,16 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   };
 
+  // LINKS ---------------------------------------------------------------------
+
+  var links = document.links;
+
+  for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+     if (links[i].hostname != window.location.hostname) {
+         links[i].target = '_blank';
+     }
+  };
+
 });
 
 // STATS -----------------------------------------------------------------------
